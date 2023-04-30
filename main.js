0d62b647eb9a4a6475f93fd02595b96164fdb635
TheoThibodeau <teddythibodeau@gmail.com>
2023-04-30 13:20:25 -0400

    updates

--- main.js
+++ main.js
@@ -1,25 +1,69 @@
-let mainContainer = document.querySelector
+let mainContainer = document.querySelector("#mainContainer");
+console.log(mainContainer);
 
 let gitUrl = "https://api.github.com/users/TheoThibodeau";
+console.log(gitUrl);
 
 fetch(gitUrl, {
     method: "GET",
     headers: {"Content-Type": "application/json" },
 })
 
-.then((response) => {
+.then(function(response) {
     return response.json();
 })
 
-.then(function (data){
-    console.log("You have been successfully subscribed", data)
-});
-
-let gitHubId = document.createElement('div');
+.then(function(info) {
+    
+    //profile pic
+    let firstDiv = document.createElement('div');
 
     let gitHubPic = document.createElement('img');
-    gitHubPic.src = gitUrl.avatar_url;
-    // apply style to a class in css first, then add that class to the element with JS
-    gitHubId.appendChild(gitHubPic);
+    gitHubPic.src = info.avatar_url;
+    gitHubPic.classList.add("gitHubPic");
+
+    firstDiv.appendChild(gitHubPic);
+    mainContainer.appendChild(firstDiv);
+
+    //location & personal info
+    let secondDiv = document.createElement('div');
+    let locationGit = document.createElement('p');
     
+    locationGit.innerText = `Location : ${info.location} \n gitUrl: TheoThibodeau \n GitHub Username: TheoThibodeau`
+    console.log(locationGit);
+
+    secondDiv.appendChild(locationGit);
+    mainContainer.appendChild(secondDiv);
+
+    //GitHub Repos
+});
+
+.then(function(info){
+    fetch(, {
+        method: "GET",
+        headers: {"Content-Type": "application/json" },)
+});
+
+
+
+
+
+
+
+
+
+
+// let createUserCard = function(userData) {
+//     let query = ["name", "avatar_url", "location", "html_url", "login", "repos_url_"]
+
+// for (let type of query){
+//     let 
+// }
+    
+// };
+
+
+// let gitHubId = document.createElement('div');
+
+
     
