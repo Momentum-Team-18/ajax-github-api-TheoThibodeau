5cf148a5e8620dcef03981b2dad906579535d129
TheoThibodeau <teddythibodeau@gmail.com>
2023-05-01 13:25:56 -0400

    CSS is fixed

--- main.js
+++ main.js
@@ -28,9 +28,15 @@ fetch(gitUrl, {
 
     //location & personal info
     let locationGit = document.createElement('h5');
-    locationGit.innerText = `Location : ${info.location} \n GitHub Username: ${info.login} \n GitHub Url: ${info.url}`;
+    locationGit.innerText = `Location : ${info.location} \n GitHub Username: ${info.login} \n GitHub Url: `;
 
     secondDiv.appendChild(locationGit);
+
+    let gitUrl = document.createElement('a');
+    gitUrl.href = info.html_url;
+    gitUrl.innerText = info.html_url;
+    gitUrl.classList.add('gitUrl');
+    locationGit.appendChild(gitUrl);
 }
 );
    
@@ -52,5 +58,4 @@ for(let i=0; i < info.length; i++) {
     repoLink.href = info[i].html_url;
     thirdDiv.appendChild(repoLink);
 };
-
 });
